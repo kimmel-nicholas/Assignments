7b9183a2dd188e48c8a7730cfd2a8c5b1722f1ae
Nicholas Kimmel <kimmel_nicholas@columbusstate.edu>
2020-11-01 00:42:56 +0000

    more edits on assignment 2

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function passwordLength() {
   }
   return true;
 }
+function hamburger() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
